2adfdadad0aa0a6661cd9bc34b8d3c8736115c80
insider-avinash <gavandheavinash12@gmail.com>
2022-12-14 07:57:08 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Boards from "./TicTacToeGame/Boards";
+
+function App() {
+  return (
+    <div className="App">
+      <Boards />
+    </div>
+  );
+}
+
+export default App;
